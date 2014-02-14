1b7ac4b234d44329b5d3501e2f5d7cf82e831ade
avani <avani9330@gmail.com>
2014-02-14 19:12:36 +0530

    Home page modified

--- bookinfo/views.py
+++ bookinfo/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response,render
+from django.views.decorators.cache import cache_page
+
+@cache_page(60*20)
 def home(request):
-    return render_to_response('base.html')
+    return render_to_response('home.html')
